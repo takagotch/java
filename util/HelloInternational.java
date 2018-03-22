ff995ca710055233f1633be46379e254c3d3339c
takagotch <dyaccb@gmail.com>
2018-03-23 03:17:57 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ util/HelloInternational.java
@@ -0,0 +1,19 @@
+//public static final ResourceBundle getBundle(
+//  String baseName[, ResourceBundle.Control control |
+//  Locale targetLocale])
+//public static final ResourceBundle getBundle(
+//  String baseName, Locle targetLocale,
+//  ResourceBundle.Control control)
+//public static ResourceBundle getBundle(String baseName,
+//  Locale targetLocale, ClassLoader loader)
+//public static ResourceBundle getBundle(String baseName,
+//  Locale targetLocale, ClassLoader loader,
+//  ResourceBundle.Control control)
+
+ResourceBundle helloResource = ResourceBundle.getBundle();
+System.out.println();
+
+ResourceBundle helloResource_fr = ResourceBundle.getBundle();
+System.out.println();
+
+
