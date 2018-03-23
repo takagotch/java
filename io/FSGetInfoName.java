34bf63793a35f7453e7dca70a08ac9685028e336
takagotch <dyaccb@gmail.com>
2018-03-24 01:17:27 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- io/FSGetInfoName.java
+++ io/FSGetInfoName.java
@@ -1,3 +1,14 @@
-//
+//public String getName()
+//public String getPath()
+//public String getPath()
+//public File getParentFile()
+//public File getAbsoluteFile()
+//public String getAbsolutePath()
+//public File getCanonicalFile()
+//public String getConoicalPath()
+
+File file = new File("chap5/data/fsFile.txt");
+System.out.println("FILE: " + file.getName());
+
 
 
