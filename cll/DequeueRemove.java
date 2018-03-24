1feda618ae4726f938f38afc186f72e09de234e6
takagotch <dyaccb@gmail.com>
2018-03-25 06:04:53 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ cll/DequeueRemove.java
@@ -0,0 +1,13 @@
+//public E pollFirst()
+//public E pollLast()
+//public E removeFirst()
+//public E removeLast()
+
+Deque<Integer> deq = new ArrayDeque<>(Arrays.asList(1, 2, 3));
+System.out.println("Deque: " + deq);
+System.out.println(deq.removeFirst());
+System.out.println(deq.removeFirst());
+System.out.println(deq.pollFisrt());
+System.out.println(deq.pollLast());
+
+
