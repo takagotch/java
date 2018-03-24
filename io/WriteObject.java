f80c9e34f5d813d8cc1b4cc994b2a6cf5c0c9975
takagotch <dyaccb@gmail.com>
2018-03-25 01:44:12 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- io/WriteObject.java
+++ io/WriteObject.java
@@ -1,5 +1,23 @@
 //public final void writeObject(Object obj)
 
-class PrintMessage implements Serializable{}
+class PrintMessage implements Serializable{
+  private static final serialVersionUID = 1L;
+  public void doPrint(String name){
+    System.out.println(name + "");
+  }
+}
 
+try{
+  String fn = "chap5/data/obTemp.txt";
+
+  try(FileOutputStream o = new FileOutputStream(fn);
+      ObjectOutputStream oObj = new ObjectOutputStream(o)){
+    oOj.writeObject((Object) (new PrintMessage()));
+      }
+  try(FileInputStream i = new FileInputStream(fn);
+      ObjectInputStream iObj = new ObjectInputStream(i)){
+      PrintMessage pm = (PrintMessage) iObj.readObject();
+      pm.doPrint("");
+      }
+}
 
