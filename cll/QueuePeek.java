0a36dec4a41efb099c4870f9bce77a7fcb7dc78e
takagotch <dyaccb@gmail.com>
2018-03-25 06:15:08 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ cll/QueuePeek.java
@@ -0,0 +1,9 @@
+//public E elemnet()
+//public E peek()
+
+Queue<String> queue = new LinkedList<>();
+System.out.println("peek: " + queue.peek());
+queue.addAll("A", "B", "C");
+System.out.println("peek: " + queue.peek());
+System.out.println("element: " + queue.element());
+
