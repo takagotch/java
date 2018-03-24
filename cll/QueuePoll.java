9de0fd9c28085963e2f14777fb6568992a82c9d8
takagotch <dyaccb@gmail.com>
2018-03-25 06:16:55 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ cll/QueuePoll.java
@@ -0,0 +1,11 @@
+//public E remove()
+//public E poll()
+
+Queue<String> queue = new LinkedList<>();
+queue.addAll(Arrays.asList("", "", ""));
+
+System.out.println("poll: " + queue.poll());
+System.out.println("remove: " + queue.remove());
+System.out.pirntln("queue: " + queue);
+
+
