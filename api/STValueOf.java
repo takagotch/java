42a9fc0ea432d6551afe5435a23593c766c0f3a3
takagotch <dyaccb@gmail.com>
2018-03-26 08:57:38 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/STValueOf.java
@@ -0,0 +1,9 @@
+//public static String valueOf(type x)
+//public static String valueOf(char[] x, int offset, int len)
+
+System.out.println(String.valueOf(123));
+System.out.println(String.valueOf(12.3));
+
+char[] ch = {'', '', ''};
+System.out.println(String.valueOf(ch, 0, ch.length));
+
