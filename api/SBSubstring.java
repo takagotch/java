0400940bc02f4ece6ab49782b3040ab9b0b1e2a6
takagotch <dyaccb@gmail.com>
2018-03-26 08:30:04 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/SBSubstring.java
@@ -0,0 +1,8 @@
+//public CharSequence subSequence(int start, int end)
+//public String substring(int start[, int end])
+
+StringBuilder sb = new StringBuilder("Hello");
+System.out.println(sb.subSequence(0, 5));
+System.out.println(sb.substring(6));
+
+
