c14460941a71ce60588621ae4ed57875f5a5f0fe
takagotch <dyaccb@gmail.com>
2018-03-26 07:57:49 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/CHToXxx.java
@@ -0,0 +1,8 @@
+//public static char toLowerCase(char ch)
+//public static char toTitleCase(char ch)
+//public static char toUpperCase(char ch)
+
+System.out.println("a" + Character.toLowerCase('A'));
+System.out.println("Z" + Character.toUpperCase('z'));
+System.out.println("l TITLECASE" +
+	Character.toTitleCase('l'));
