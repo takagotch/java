9f667751bacdc8cfad62ed75abcaf2bf0baa8572
takagotch <dyaccb@gmail.com>
2018-03-26 08:18:49 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/Exp.java
@@ -0,0 +1,6 @@
+//public static dobule exp(double a)
+//public static double pow(double a, dobule b)
+
+System.out.println("e 3" + Math.exp(5));
+System.out.println("4 3" + Math.pow(4, 3));
+
---
+++ api/Log.java
@@ -0,0 +1,10 @@
+//public static double log(double a)
+//public static double log10(double a)
+//public staitc double log1p(double a)
+
+double a = 2;
+
+System.out.println(Math.log(a));
+System.out.println(Math.log10(a));
+System.out.println(Math.log1p(a));
+
