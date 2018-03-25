0c58295ffb5c02b36d36b23d3a126f12e51053db
takagotch <dyaccb@gmail.com>
2018-03-26 08:05:58 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/ValueOf.java
@@ -0,0 +1,19 @@
+//public static Boolean valueOf(boolean x)
+//public static Byte valueOf(byte x)
+//public static Double valueOf(double x)
+//public static Flaot valueOf(float x)
+//public static Integer valueOf(int)
+//public static Long valueOf(long x)
+//public static Short valueOf(short x)
+
+Flaot fl = Float.valueOf(1.23f);
+Boolean bl2 = Boolean.valueOf(false);
+System.out.println(fl.toString());
+System.out.println(bl1.toString());
+
+Double d = 2d;
+while(d <= 10){
+  d += 1.5;
+}
+System.out.println();
+
