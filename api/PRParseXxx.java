1fc43e52ca09fe4672b13a6ae1a96eda39ba4c56
takagotch <dyaccb@gmail.com>
2018-03-26 08:00:13 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/PRParseXxx.java
@@ -0,0 +1,10 @@
+//public static byte parseByte(String s[, int radix])
+//public static double parseDouble(String s)
+//public static float parseFloat(String s)
+//public static int parseInt(String s[, int radix])
+//public static long parseLong(String s[, int radix])
+//public static short parseLong(String s[, int radix])
+
+System.out.println(Float.parseFloat("913"));
+System.out.println(Integer.parseInt("1010", 2));
+
