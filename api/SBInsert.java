7c3f60786d73878251d65ebb90b1501166738bf7
takagotch <dyaccb@gmail.com>
2018-03-26 08:51:33 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/SBInsert.java
@@ -0,0 +1,7 @@
+//public buffer insert(int index, type x)
+//public buffer insert(int index, char[] x, int offset, int len)
+//public buffer insert(int dstOffset, CharSequence s [, int start, int end])
+
+StringBuilder sb = new StringBuilder("");
+System.out.println(sb.insert(3, 2018));
+
---
+++ api/SBReplace.java
@@ -0,0 +1,6 @@
+//public buffer replace(int start, int end, String s)
+
+StringBuilder sb = new StringBuilder("");
+System.out.println(sb.replace(5, 8, ""));
+
+
