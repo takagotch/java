37b4b20c5cf8922f3e62d7a79e8c8c74e36f8b6e
takagotch <dyaccb@gmail.com>
2018-03-26 06:44:36 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/thread.java
@@ -0,0 +1,13 @@
+//public static void yield()
+//public void interrupt()
+
+//public final void notify()
+//public final void notifyAll()
+//public final void join()
+//public final void join(long millis[, int nanos])
+
+//public final void wait([long millis[, int nanos]])
+
+
+
+
