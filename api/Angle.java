82fbd6f2334670c9450add347eab34c7a2add5c0
takagotch <dyaccb@gmail.com>
2018-03-26 08:16:01 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/Angle.java
@@ -0,0 +1,8 @@
+//public static double toRadians(double angdeg)
+//public static double toDegrees(double angrad)
+
+double s = 45;
+System.out.println(Math.toRadiand(a));
+System.out.println(Math.toDegrees(Math.toRadians(a)));
+
+
---
+++ api/Sin.java
@@ -0,0 +1,12 @@
+//public static double sin(double a)
+//public static dobule cos(double a)
+//public static double tan(double a)
+//public static double asin(double a)
+//public static double acos(double a)
+//public static double atan(double a)
+
+double s = Math.sin(Math.toRadians(a));
+
+double as = Math.toDegrees(Math.asin(s));
+System.out.print(as);
+
