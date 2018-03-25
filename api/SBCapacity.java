fb6691268e7d757c62cf672dbff2a59c5208e869
takagotch <dyaccb@gmail.com>
2018-03-26 08:47:06 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/SBCapacity.java
@@ -0,0 +1,10 @@
+//public int capacity()
+//public void ensureCapacity(int minimumCapacity)
+
+StringBuilder sb = new StringBuilder("Java");
+
+System.out.println(sb.capacity());
+sb.ensureCapacity(30);
+
+System.out.println(sb.capacity());
+
