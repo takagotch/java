306d6cebf6b254a5c90fdf918d0c0a6c513d7ca5
takagotch <dyaccb@gmail.com>
2018-03-26 07:52:49 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

---
+++ api/CHDigit.java
@@ -0,0 +1,6 @@
+//public static int digit(char ch, int radix)
+//public static char forDigit(int digit, int radiz)
+
+System.out.println(Character.digit('D', 16));
+System.out.println(Character.forDigit(13, 16));
+
